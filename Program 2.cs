f502744ce4cb3f1d00d17d53d2494a8025192516
Jemima <jemimaabu@gmail.com>
2017-08-10 21:57:08 +0100

    Add files via upload

---
+++ Program 2.cs
@@ -0,0 +1,411 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GP_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Score1;
+            Console.Write("Score 1 : ");
+            Score1 = Convert.ToInt32(Console.ReadLine());
+            double Unit1;
+            Console.Write("Unit : ");
+            Unit1 = Convert.ToInt32(Console.ReadLine());
+            double Course1;
+            if (Score1 >= 70)
+            {
+                Course1 = Unit1 * 5;
+            }
+            else if (Score1 >= 60)
+            {
+                Course1 = Unit1 * 4;
+            }
+            else if (Score1 >= 50)
+            {
+                Course1 = Unit1 * 3;
+            }
+            else if (Score1 >= 45)
+            {
+                Course1 = Unit1 * 2;
+            }
+            else if (Score1 >= 40)
+            {
+                Course1 = Unit1 * 1;
+            }
+            else
+            {
+                Course1 = Unit1 * 0;
+            }
+
+            Console.Write("Course = "); Console.WriteLine(Course1);
+            double Score2;
+            Console.Write("Score 2 : ");
+            Score2 = Convert.ToDouble(Console.ReadLine());
+            double Unit2;
+            Console.Write("Unit : ");
+            Unit2 = Convert.ToDouble(Console.ReadLine());
+            double Course2;
+            if (Score2 >= 70)
+            {
+                Course2 = Unit2 * 5;
+            }
+            else if (Score2 >= 60)
+            {
+                Course2 = Unit2 * 4;
+            }
+            else if (Score2 >= 50)
+            {
+                Course2 = Unit2 * 3;
+            }
+            else if (Score2 >= 45)
+            {
+                Course2 = Unit2 * 2;
+            }
+            else if (Score2 >= 40)
+            {
+                Course2 = Unit2 * 1;
+            }
+            else
+            {
+                Course2 = Unit2 * 0;
+            }
+
+            Console.Write("Course = "); Console.WriteLine(Course2);
+            double Score3;
+            Console.Write("Score 3 : ");
+            Score3 = Convert.ToDouble(Console.ReadLine());
+            double Unit3;
+            Console.Write("Unit : ");
+            Unit3 = Convert.ToDouble(Console.ReadLine());
+            double Course3;
+            if (Score3 >= 70)
+            {
+                Course3 = Unit3 * 5;
+            }
+            else if (Score3 >= 60)
+            {
+                Course3 = Unit3 * 4;
+            }
+            else if (Score3 >= 50)
+            {
+                Course3 = Unit3 * 3;
+            }
+            else if (Score3 >= 45)
+            {
+                Course3 = Unit3 * 2;
+            }
+            else if (Score3 >= 40)
+            {
+                Course3 = Unit3 * 1;
+            }
+            else
+            {
+                Course3 = Unit3 * 0;
+            }
+
+            Console.Write("Course = "); Console.WriteLine(Course3);
+            double Score4;
+            Console.Write("Score 4 : ");
+            Score4 = Convert.ToDouble(Console.ReadLine());
+            double Unit4;
+            Console.Write("Unit : ");
+            Unit4 = Convert.ToDouble(Console.ReadLine());
+            double Course4;
+            if (Score4 >= 70)
+            {
+                Course4 = Unit4 * 5;
+            }
+            else if (Score4 >= 60)
+            {
+                Course4 = Unit4 * 4;
+            }
+            else if (Score4 >= 50)
+            {
+                Course4 = Unit4 * 3;
+            }
+            else if (Score4 >= 45)
+            {
+                Course4 = Unit4 * 2;
+            }
+            else if (Score4 >= 40)
+            {
+                Course4 = Unit4 * 1;
+            }
+            else
+            {
+                Course4 = Unit4 * 0;
+            }
+
+            Console.Write("Course = "); Console.WriteLine(Course4);
+            double Score5;
+            Console.Write("Score 5 : ");
+            Score5 = Convert.ToDouble(Console.ReadLine());
+            double Unit5;
+            Console.Write("Unit : ");
+            Unit5 = Convert.ToDouble(Console.ReadLine());
+            double Course5;
+            if (Score5 >= 70)
+            {
+                Course5 = Unit5 * 5;
+            }
+            else if (Score5 >= 60)
+            {
+                Course5 = Unit5 * 4;
+            }
+            else if (Score5 >= 50)
+            {
+                Course5 = Unit5 * 3;
+            }
+            else if (Score5 >= 45)
+            {
+                Course5 = Unit5 * 2;
+            }
+            else if (Score5 >= 40)
+            {
+                Course5 = Unit5 * 1;
+            }
+            else
+            {
+                Course5 = Unit5 * 0;
+            }
+
+            Console.Write("Course = "); Console.WriteLine(Course5);
+            double Score6;
+            Console.Write("Score 6 : ");
+            Score6 = Convert.ToDouble(Console.ReadLine());
+            double Unit6;
+            Console.Write("Unit : ");
+            Unit6 = Convert.ToDouble(Console.ReadLine());
+            double Course6;
+            if (Score6 >= 70)
+            {
+                Course6 = Unit6 * 5;
+            }
+            else if (Score6 >= 60)
+            {
+                Course6 = Unit6 * 4;
+            }
+            else if (Score6 >= 50)
+            {
+                Course6 = Unit6 * 3;
+            }
+            else if (Score6 >= 45)
+            {
+                Course6 = Unit6 * 2;
+            }
+            else if (Score6 >= 40)
+            {
+                Course6 = Unit6 * 1;
+            }
+            else
+            {
+                Course6 = Unit6 * 0;
+            }
+
+            Console.Write("Course = "); Console.WriteLine(Course6);
+            double Score7;
+            Console.Write("Score 7 : ");
+            Score7 = Convert.ToDouble(Console.ReadLine());
+            double Unit7;
+            Console.Write("Unit : ");
+            Unit7 = Convert.ToDouble(Console.ReadLine());
+            double Course7;
+            if (Score7 >= 70)
+            {
+                Course7 = Unit7 * 5;
+            }
+            else if (Score7 >= 60)
+            {
+                Course7 = Unit7 * 4;
+            }
+            else if (Score7 >= 50)
+            {
+                Course7 = Unit7 * 3;
+            }
+            else if (Score7 >= 45)
+            {
+                Course7 = Unit7 * 2;
+            }
+            else if (Score7 >= 40)
+            {
+                Course7 = Unit7 * 1;
+            }
+            else
+            {
+                Course7 = Unit7 * 0;
+            }
+
+            Console.Write("Course = "); Console.WriteLine(Course7);
+            double Score8;
+            Console.Write("Score 8 : ");
+            Score8 = Convert.ToDouble(Console.ReadLine());
+            double Unit8;
+            Console.Write("Unit : ");
+            Unit8 = Convert.ToDouble(Console.ReadLine());
+            double Course8;
+            if (Score8 >= 70)
+            {
+                Course8 = Unit8 * 5;
+            }
+            else if (Score8 >= 60)
+            {
+                Course8 = Unit8 * 4;
+            }
+            else if (Score8 >= 50)
+            {
+                Course8 = Unit8 * 3;
+            }
+            else if (Score8 >= 45)
+            {
+                Course8 = Unit8 * 2;
+            }
+            else if (Score8 >= 40)
+            {
+                Course8 = Unit8 * 1;
+            }
+            else
+            {
+                Course8 = Unit8 * 0;
+            }
+
+            Console.Write("Course = "); Console.WriteLine(Course8);
+            double Score9;
+            Console.Write("Score 9 : ");
+            Score9 = Convert.ToDouble(Console.ReadLine());
+            double Unit9;
+            Console.Write("Unit : ");
+            Unit9 = Convert.ToDouble(Console.ReadLine());
+            double Course9;
+            if (Score9 >= 70)
+            {
+                Course9 = Unit9 * 5;
+            }
+            else if (Score9 >= 60)
+            {
+                Course9 = Unit9 * 4;
+            }
+            else if (Score9 >= 50)
+            {
+                Course9 = Unit9 * 3;
+            }
+            else if (Score9 >= 45)
+            {
+                Course9 = Unit9 * 2;
+            }
+            else if (Score9 >= 40)
+            {
+                Course9 = Unit9 * 1;
+            }
+            else
+            {
+                Course9 = Unit9 * 0;
+            }
+
+            Console.Write("Course = "); Console.WriteLine(Course9);
+            double Score10;
+            Console.Write("Score 10 : ");
+            Score10 = Convert.ToDouble(Console.ReadLine());
+            double Unit10;
+            Console.Write("Unit : ");
+            Unit10 = Convert.ToDouble(Console.ReadLine());
+            double Course10;
+            if (Score10 >= 70)
+            {
+                Course10 = Unit10 * 5;
+            }
+            else if (Score10 >= 60)
+            {
+                Course10 = Unit10 * 4;
+            }
+            else if (Score10 >= 50)
+            {
+                Course10 = Unit10 * 3;
+            }
+            else if (Score10 >= 45)
+            {
+                Course10 = Unit10 * 2;
+            }
+            else if (Score10 >= 40)
+            {
+                Course10 = Unit10 * 1;
+            }
+            else
+            {
+                Course10 = Unit10 * 0;
+            }
+
+            Console.Write("Course = "); Console.WriteLine(Course10);
+
+            double Carryover;
+            Console.Write("Do you have any carry over course (1/0)?");
+            Carryover = Convert.ToDouble(Console.ReadLine());
+            if (Carryover <= 0)
+            {
+                Console.WriteLine();
+            }
+                double Unit;
+                Console.Write("Unit : ");
+                Unit = Convert.ToDouble(Console.ReadLine());
+                double Score;
+                Console.Write("Score: ");
+                Score = Convert.ToDouble(Console.ReadLine());
+                double Years;
+                Console.Write("How many years have you had this course?");
+                Years = Convert.ToDouble(Console.ReadLine());
+                if (Score >= 70)
+                {
+                    Carryover = Unit * (5 / Years);
+                }
+                else if (Score >= 60)
+                {
+                    Carryover = Unit * (4 / Years);
+                }
+                else if (Score >= 50)
+                {
+                    Carryover = Unit * (3 / Years);
+                }
+                else if (Score >= 45)
+                {
+                    Carryover = Unit * (2 / Years);
+                }
+                else if (Score >= 40)
+                {
+                    Carryover = Unit * (1 / Years);
+                }
+                else
+                {
+                    Carryover = Unit * 0;
+                }
+
+                Console.Write("Course = "); Console.WriteLine(Carryover);
+            
+
+
+            double TotalUnits = Unit1 + Unit2 + Unit3 + Unit4 + Unit5 + Unit6 + Unit7 + Unit8 + Unit9 + Unit10 + Unit;
+            Console.Write("Total Units = "); Console.WriteLine(TotalUnits);
+            double TotalScore = Course1 + Course2 + Course3 + Course4 + Course5 + Course6 + Course7 + Course8 + Course9 + Course10 + Carryover;
+            Console.Write("Total Units Scored = "); Console.WriteLine(TotalScore);
+            double GPA = TotalScore / TotalUnits;
+            Console.Write("Grade Point Average = "); Console.WriteLine(GPA);
+            Console.WriteLine("");
+
+            if (GPA >= 4.5)
+            { Console.WriteLine("Congratulations! You're on a First Class"); }
+            else if (GPA >= 3.5)
+            { Console.WriteLine("Well done! Second Class Upper"); }
+            else if (GPA >= 3)
+            { Console.WriteLine("Second Class Lower. That's... nice."); }
+            else if (GPA >= 2.5)
+            { Console.WriteLine("Third Class. Err... better luck next time?"); }
+            else
+            { Console.WriteLine("You know, school isn't for everybody. Do you really want to be here?"); }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
